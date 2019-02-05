ea8449f0b83d9a2847c312aea2e514826d559b79
Steve Sanders <steve@keyba.se>
2019-02-04 20:12:57 -0800

    comment

--- shared/tracker2/index.desktop.js
+++ shared/tracker2/index.desktop.js
@@ -132,7 +132,7 @@ const Tracker = (props: Props) => {
 
   const buttons = getButtons(props)
 
-  // In order to keep the 'effect' of the card sliding up on top of the text the text is below the scroll area. We still need the spacing so we draw the text inside the scroll but invisible
+  // Keep the header outside the scrollable area so the close button stays uncovered
 
   return (
     <Kb.Box2 direction="vertical" fullWidth={true} fullHeight={true} style={styles.container}>
